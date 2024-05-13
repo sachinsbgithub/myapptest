f65d33356a007bdec423c77938860e04bf3f95d7
sachinsb <sachinsb@mobil80.com>
2024-05-13 22:18:56 +0530

    correted repo name

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
-  base: '/myapp',
+  base: '/myapptest',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
